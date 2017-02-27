d4d6a4286dce7dabe91e7916ef5ac7de5559a524
Ravon L Wright <flyr397@gmail.com>
2017-02-27 09:06:59 -0500

    added
    added the program

--- triangleApplication/Program.cs
+++ triangleApplication/Program.cs
@@ -1,15 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace triangleApplication
 {
-    class Program
+    class Triangle
+    {
+
+        double Height;
+        double Base;
+
+
+        public void AcceptDetails()
+        {
+            Height = 16.5;
+            Base = 16.5;
+        }
+
+        public double GetArea()
+        {
+            return Height * Base / 2;
+        }
+
+        public void Display()
+        {
+            WriteLine("Base : {0}", Height);
+            WriteLine("Height : {0}", Base);
+            WriteLine("Area : {0}", GetArea());
+            ReadKey();
+        }
+    }
+
+    class ExecuteSquare
     {
         static void Main(string[] args)
         {
+            Triangle t = new Triangle();
+
+            t.AcceptDetails();
+            t.Display();
+            ReadLine();
         }
     }
 }
+
